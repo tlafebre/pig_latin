e3cc829edd4a20f50c0f3e851b660902c2c09fcd
Tjeerd Lafebre <tjeerd.lafebre@gmail.com>
2019-06-30 12:31:28 +0200

    done

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,30 @@
   to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
 */
 
-fn starts_with_consonant(word: String) -> bool {
-    vec!["a", "e", "i", "o", "u"].into_iter().any(|c| c == &word[0..1].to_lowercase())
+fn is_vowel(letter: &str) -> bool {
+    vec!["a", "e", "i", "o", "u"].into_iter().any(|c| c == &letter.to_lowercase())
+}
 
+fn pigify_one(word: String) -> String {
+    let first_letter = &word[0..1];
+    if is_vowel(first_letter) {
+        format!("{}-hay", &word)
+    } else {
+        format!("{}-{}ay", &word[1..], &first_letter)
+    }
+}
+
+fn pigify(string: &str) {
+    let mut new_str = String::from("");
+
+    for word in string.split_whitespace() {
+        new_str.push_str(&(pigify_one(word.to_string())));
+        new_str.push(' ');
+    }
+    println!("{}", &new_str);
 }
 
 fn main() {
-    let word = String::from("Tjeerd");
-
-    let word2 = String::from("apple");
-
-    let word3 = String::from("Apple");
+    let word = String::from("Tjeerd likes to write in Rust and eat a lot of apples");
+    pigify(&word);
 }
-
